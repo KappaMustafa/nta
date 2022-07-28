aebe470ede5ec24eb94c56c22c521dca7a2b9d25
KappaMustafa <steef136@gmail.com>
2022-07-28 06:17:12 -0500

    added writefile and other stuff but /notes still not working

--- routes/notes_routes.js
+++ routes/notes_routes.js
@@ -1,33 +1,41 @@
 const notes_router = require('express').Router()
+const html_router = require('express').Router();
+const html_router2 = require('express').Router();
 const fs = require('fs')
 const path = require('path')
+const uuid = require ('uuid').v4
 
-notes_router.get('/notes', (req, res) =>{
+html_router.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname,'../public/index.html'))
+});
+
+html_router2.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname,'../public/notes.html'))
+});
+
+
+
+notes_router.get('/api/notes', (req, res) =>{
     fs.readFile(path.join(__dirname, '../db/db.json' ), 'utf-8', (err, data) =>{
         if(err) return console.log(err);
 
         res.json(JSON.parse(data))
     })
-    
-    res.send('y')
 })
 
-notes_router.post('/notes', (req, res) =>{
+notes_router.post('/api/notes', (req, res) =>{
 
-    let recievedNote = {
-        title: '',
-        text: ''
-    }
     fs.readFile(path.join(__dirname, '../db/db.json' ), 'utf-8', (err, data) =>{
         if(err) return console.log(err);
 
         res.json(JSON.parse(data))
     })
-    
-    res.send('x')
+    fs.writeFile('./db/db.json', (err, data) => {
+        if (err) console.log(err);
+    });
 })
 
 
 
 
-module.exports = notes_router;
+module.exports = notes_router, html_router, html_router2;
--- server.js
+++ server.js
@@ -3,10 +3,13 @@ const app = express();
 const PORT = process.env.PORT || 3333
 const path = require('path');
 const notes_router = require('./routes/notes_routes');
+const html_router = require('./routes/notes_routes');
+const html_router2 = require('./routes/notes_routes');
 
 app.use(express.static(path.join(__dirname, 'public')))
-
-app.use('/api',notes_router)
+app.use('/', html_router);
+app.use('/notes', html_router2);
+app.use('/api', notes_router)
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
 
