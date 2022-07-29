f98e81a666ed57fd9ab4b7d2f2a26d62f8c10613
KappaMustafa <steef136@gmail.com>
2022-07-29 00:26:01 -0500

    fixed routing issues

--- routes/notes_routes.js
+++ routes/notes_routes.js
@@ -1,41 +1,56 @@
 const notes_router = require('express').Router()
 const html_router = require('express').Router();
-const html_router2 = require('express').Router();
 const fs = require('fs')
 const path = require('path')
 const uuid = require ('uuid').v4
 
+// HTML ROUTERS 
 html_router.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname,'../public/index.html'))
-});
-
-html_router2.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname,'../public/notes.html'))
-});
-
-
-
-notes_router.get('/api/notes', (req, res) =>{
-    fs.readFile(path.join(__dirname, '../db/db.json' ), 'utf-8', (err, data) =>{
+    
+    res.sendFile(path.join(__dirname,'../public/index.html'),'utf-8', (err, data) =>{
         if(err) return console.log(err);
 
-        res.json(JSON.parse(data))
     })
-})
-
-notes_router.post('/api/notes', (req, res) =>{
-
-    fs.readFile(path.join(__dirname, '../db/db.json' ), 'utf-8', (err, data) =>{
+});
+html_router.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/notes.html'), 'utf-8', (err, data) =>{
         if(err) return console.log(err);
-
-        res.json(JSON.parse(data))
     })
-    fs.writeFile('./db/db.json', (err, data) => {
+});
+
+
+
+//API ROUTERS
+notes_router.get('/notes', (req, res) => {
+
+    fs.readFile('./db/db.json', (err, data) => {
         if (err) console.log(err);
+
+        let dbData = JSON.parse(data);
+
+        res.send(dbData);
+    })
+});
+notes_router.post('/notes', (req, res) => {
+    const userNote = {
+        title: req.body.title,
+        text: req.body.text
+    };
+    fs.readFile('./db/db.json', (err, data) => {
+        if (err) console.log(err);
+        const notesDb = JSON.parse(data);
+        notesDb.push(userNote);
+        notesDb.forEach((note, index) => {
+            const id = uuid().slice(0, 4);
+            note.id = id;
+            return notesDb;
+        });
+        const turnNotesToStrings = JSON.stringify(notesDb);
+        fs.writeFile('./db/db.json', turnNotesToStrings, (err, data) => {
+            if (err) console.log(err);
+        });
     });
-})
+});
 
 
-
-
-module.exports = notes_router, html_router, html_router2;
+module.exports = {notes_router: notes_router, html_router: html_router};
--- server.js
+++ server.js
@@ -2,16 +2,15 @@ const express = require('express')
 const app = express();
 const PORT = process.env.PORT || 3333
 const path = require('path');
-const notes_router = require('./routes/notes_routes');
-const html_router = require('./routes/notes_routes');
-const html_router2 = require('./routes/notes_routes');
+const routes = require('./routes/notes_routes')
+     
+
 
 app.use(express.static(path.join(__dirname, 'public')))
-app.use('/', html_router);
-app.use('/notes', html_router2);
-app.use('/api', notes_router)
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
+app.use('/', routes.html_router);
+app.use('/api', routes.notes_router)
 
 app.listen(PORT, () =>{
     console.log(`Listening on port ${PORT}`);
